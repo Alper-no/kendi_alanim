28d724447c8e49e986c960d2d79a61e673054f2a
Alper <alper.ccgn@gmail.com>
2023-05-16 19:49:57 +0200

    v1

--- js01-intro/js6/scope.js
+++ js01-intro/js6/scope.js
@@ -2,3 +2,12 @@
 // ?              SCOPE
 // ? ======================================
 console.log("******* SCOPE ********")
+
+let number1 = 3
+console.log(number1); //3
+
+const funk1 = () => {
+    number1= 44
+} 
+funk1()
+console.log(number1);
