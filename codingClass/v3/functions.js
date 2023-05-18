b39484194d7c4c94f873b704070be78dd6eec045
Alper <alper.ccgn@gmail.com>
2023-05-18 18:32:54 +0200

    do it

--- codingClass/v3/functions.js
+++ codingClass/v3/functions.js
@@ -51,28 +51,30 @@
 
 // 4- Belirli bir string ifadenin içindeki sesli harfleri bulan fonksiyonu yazınız?(includes)
 
-// console.log(findVowels('hello world')) // output: eoo
-​
-​
-​
+console.log(findVowels('hello world'))// output: eoo
+
+
+
+
+
 // 5- Belirli bir sayının tam bölenlerinin toplamını bulan fonksiyonu yazınız?
 // console.log(sumOfDivisors(12)) // output: 28
-​
+
 // 6- Belirli bir sayının asal olup olmadığını bulan fonksiyonu yazınız?
 // console.log(isPrime(7)) // output: true
-​
+
 // 7- Bir sayının faktöriyelini hesaplayan bir fonksiyon yazınız.
 // console.log(factorial(5)) // output: 120
-​
+
 // 8- Bir dizi içerisindeki en büyük sayıyı bulan bir fonksiyon yazınız.
 // console.log(findMax([1, 2, 3, 4, 5, 6, 7, 8, 9])) // output: 9
-​
+
 // 9- Bir dizi içerisindeki sayıları toplayan bir fonksiyon yazınız.
 // console.log(sumOfArray([1, 2, 3, 4, 5, 6, 7, 8, 9])) // output: 45
-​
+
 // 10-Girilen bir cümledeki kelime sayısını bulan bir fonksiyon yazınız.
-​
+
 // console.log(findWordCount('hello world')) // output: 2
-​
+
 // 11- Girilen bir sayının pozitif çarpanlarını bulan bir fonksiyon yazınız
 // console.log(findPositiveDivisors(12)) // output: [1, 2, 3, 4, 6, 12]
