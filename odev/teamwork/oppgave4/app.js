e722ca0935fe4662af3b8c8186039301b99bcb4f
Alper <alper.ccgn@gmail.com>
2023-05-19 20:57:39 +0200

    v1

---
+++ odev/teamwork/oppgave4/app.js
@@ -0,0 +1,9 @@
+let text = prompt('text giriniz :')
+let result = text.split(' ')
+// console.log(result);
+// console.log(result[0]);
+let sonuc = ''
+for (let i= result.length; i > 0; i--){
+    sonuc += result[i-1]+ ' '
+}
+console.log(sonuc);
