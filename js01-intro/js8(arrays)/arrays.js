b04187fa155a2711ebcfc9187b67f0b75484d790
Alper <alper.ccgn@gmail.com>
2023-05-20 10:41:53 +0200

    v2

--- js01-intro/js8(arrays)/arrays.js
+++ js01-intro/js8(arrays)/arrays.js
@@ -7,3 +7,52 @@ console.log(dizi);
 
 const dizi2 =  new Array(10)//boyle yazinca 10 tane bos array olusturur)
 console.log(dizi2);
+
+// const colors = ['Red','Green','Blue']
+
+// console.log(colors[0])
+// console.log(colors[colors.length - 1]) //son elaman
+// console.log(colors[colors.length - 2]) //sondan ikinciye ulaşım.
+// console.log(colors[-1]) //undefined
+// console.log(colors[56]) //undefined
+
+
+//Array is Array
+// const colors = ['Red','Green','Blue'];
+// console.log(typeof colors);
+
+// console.log(Array.isArray(colors)) //true 
+// console.log(colors instanceof Array) //true 
+
+
+// const variables = '1';
+// console.log(Array.isArray(variables)) //false 
+// console.log(variables instanceof Array) //false 
+
+
+//Strings immutable değiştirilemez
+// let str = 'Brown';
+// console.log(str[0])
+// str[0]='b';
+// console.log(str)
+
+// str = 'brown'
+
+//Arrayle mutable yani değiştirilebilir
+// const colors = ['Red','Green','Blue'];
+
+// colors[1] = 'yellow' //mevcudu değiştirir
+
+// console.log(colors)
+
+
+// //ekleme yapar
+// colors[3]= 5
+
+// console.log(colors)
+
+// let c = 'Pink'
+
+// colors[4] = c
+
+// console.log(colors)
