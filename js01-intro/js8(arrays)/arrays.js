fe8dda25078351ac535543ddddc64ba9d86d0a00
Alper <alper.ccgn@gmail.com>
2023-05-20 10:24:14 +0200

    v2

--- js01-intro/js8(arrays)/arrays.js
+++ js01-intro/js8(arrays)/arrays.js
@@ -1,1 +1,9 @@
 console.log('Hello World')
+//array olusurma metodlari
+const arr =['a',25,false]
+
+const dizi = new Array(100,'cw', true)
+console.log(dizi);
+
+const dizi2 =  new Array(10)//boyle yazinca 10 tane bos array olusturur)
+console.log(dizi2);
