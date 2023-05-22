a7ffe67bbe09fe38d41b7510705b990d26f046de
Alper <alper.ccgn@gmail.com>
2023-05-22 19:27:29 +0200

    v2

--- js01-intro/js9(arrays iteras.)/loop.js
+++ js01-intro/js9(arrays iteras.)/loop.js
@@ -3,3 +3,11 @@
 //* ============================================
 
 console.log("******* LOOPS IN ARRAYS ******")
+
+const grades = [55, 77, 23, 89, 100, 44, 33]
+let sum = 0
+for(let i = 0 ; i< grades.length; i++){
+    sum +=grades[i]
+}
+console.log('AVG:' ,Math.round(sum / grades.length));
+console.log('AVG:' ,(sum / grades.length).toFixed(2)); // virgulden sonra ikki basamak alir.
