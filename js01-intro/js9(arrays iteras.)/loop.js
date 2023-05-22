697c5ae6cf381cd5750935115b85dbf64e9946d7
Alper <alper.ccgn@gmail.com>
2023-05-22 19:36:45 +0200

    v3

--- js01-intro/js9(arrays iteras.)/loop.js
+++ js01-intro/js9(arrays iteras.)/loop.js
@@ -4,13 +4,14 @@
 
 console.log("******* LOOPS IN ARRAYS ******")
 
-const grades = [55, 77, 23, 89, 100, 44, 33]
-let sum = 0
-for(let i = 0 ; i< grades.length; i++){
-    sum +=grades[i]
-}
-console.log('AVG:' ,Math.round(sum / grades.length));
-console.log('AVG:' ,(sum / grades.length).toFixed(2)); // virgulden sonra ikki basamak alir.
+// const grades = [55, 77, 23, 89, 100, 44, 33]
+// let sum = 0
+// for(let i = 0 ; i< grades.length; i++){
+//     sum +=grades[i]
+// }
+
+// console.log('AVG:' ,Math.round(sum / grades.length));
+// console.log('AVG:' ,(sum / grades.length).toFixed(2)); // virgulden sonra ikki basamak alir.
 
 //?-------------- ÖRNEK -------------------
 //? grades dizisindeki notların 50'den kucuk olanları ve buyuk olanları 2 ayri diziye kaydediniz.
@@ -27,3 +28,5 @@ for(let j=0 ; j<grades.length;j++){
         equalOrBiggerThan50.push(grades[j])
     }
 }
+console.log(lessThan50);
+console.log(equalOrBiggerThan50);
