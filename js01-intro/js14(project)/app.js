24d94a2945a4cab3dd0184025c89525369d63b7a
Alper <alper.ccgn@gmail.com>
2023-06-05 20:28:08 +0200

    p2

--- js01-intro/js14(project)/app.js
+++ js01-intro/js14(project)/app.js
@@ -1,9 +1,46 @@
 //* ------ Selectors ------- */
+const selectionArticle = document.querySelector(".selection")
+
+//? Secilen elemanlarin tayicilari
+const yourChoiceDiv = document.getElementById("your-choice")
 
 //* ------- Variables ------- */
-
+let userSelectImg = document.createElement("img")
 
 //* ------- Event Listeners ------- */
+selectionArticle.addEventListener("click", (e) => {
+  console.log(e.target.id)
+  if (e.target.id) {
+    userSelectImg.src = `./assets/${e.target.id}.png`
+    userSelectImg.alt = e.target.id
+    yourChoiceDiv.appendChild(userSelectImg)
+  }
+})
 
+//? Resimler
+// const rockImg = document.getElementById("rock")
+// const paperImg = document.getElementById("paper")
+// const scissorImg = document.getElementById("scissor")
+
+// rockImg.addEventListener("click", () => {
+//   image.src = "./assets/rock.png"
+//   image.alt = "rock"
+//   yourChoiceDiv.appendChild(image)
+
+//   //? innerHTML
+//   // yourChoiceDiv.innerHTML = `<img src="./assets/rock.png" alt="rock">`
+// })
+
+// paperImg.addEventListener("click", () => {
+//   image.src = "./assets/paper.png"
+//   image.alt = "paper"
+//   yourChoiceDiv.appendChild(image)
+// })
+
+// scissorImg.addEventListener("click", () => {
+//   image.src = "./assets/scissor.png"
+//   image.alt = "scissor"
+//   yourChoiceDiv.appendChild(image)
+// })
 
 //* ------- Functions ------- */
