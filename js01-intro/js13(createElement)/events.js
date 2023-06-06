cd33e241dadb3c7dbd7cea35a343f054b0ac386d
Alper <alper.ccgn@gmail.com>
2023-06-06 21:04:26 +0200

    events1

--- js01-intro/js13(createElement)/events.js
+++ js01-intro/js13(createElement)/events.js
@@ -1,48 +1,64 @@
-document.querySelector('header h1').onmouseover = function (){
-    document.querySelector('header h1').style.color= "red"
-}
-//? Alternatif olarak usttekine
-const heading1 = document.querySelector('header h1')
+//* ==============================================
+//*                  EVENTS
+//* ===============================================
 
-heading1.onmouseover = function(){ //uzerine geldiginde
-    heading1.style.color = "red"
+console.log("****** EVENTS *******")
+
+// document.querySelector("header h1").onmouseover = function () {
+//   document.querySelector("header h1").style.color = "red"
+// }
+
+//? Alternative olarak
+const heading1 = document.querySelector("header h1")
+
+heading1.onmouseover = function () {
+  heading1.style.color = "red"
 }
 
-heading1.onmouseout = function(){ //uzerinden ciktiginda
-    heading1.style.color = "black"
+heading1.onmouseout = function () {
+  heading1.style.color = "black"
 }
-
 
 const printHello = () => {
-    document.write("Hello")
+  document.write("Hello")
 }
 
-
-//? onload event i html ve css kodlarinii render edilmesi akabinde calsir.
+//? onload event'ı html ve css kodlarinini render edilmesini akabinde calisir.
 window.onload = printHello()
 
-printHello()
-
-// her yenilenme isleminde input alanina focuslaniyor
 window.addEventListener("load", () => {
-    document.getElementById("input").focus()
+  document.getElementById("input").focus()
 })
-
-
 
 const addBtn = document.querySelector("#btn")
 const ul = document.createElement("ul")
 document.querySelector(".item-list").before(ul)
 
-addBtn.addEventListener("click", (){
-    console.log('Btn Clicked');
-    console.log(e);
-    console.log(e.target);
-    console.log(e.target.value);
+addBtn.addEventListener("click", (e) => {
+  //   console.log("Btn Clicked")
+  //   console.log(e)
+  //   console.log(e.target)
+  //   console.log(e.target.value)
 
+  if (!input.value) {
+    alert("Please enter a comment")
+  } else {
+    const input = document.querySelector("#input")
+    const li = document.createElement("li")
+    ul.appendChild(li)
+    const textLi = document.createTextNode(input.value)
+    li.appendChild(textLi)
+    input.value = ""
+    input.focus()
+  }
+})
 
-const input = document.querySelector("#input")
-
-
-
+//? input'un üzerinde iken bir klavye tusuna basilirsa calis
+document.querySelector("#input").addEventListener("keydown", (e) => {
+  //   console.log(e.code)
+  //   console.log(e.keyCode)
+  //? Basilan tus Enter ise
+  if (e.code === "Enter") {
+    addBtn.click() //? add butonun tiklanma fonksiyonu calistir.
+  }
 })
