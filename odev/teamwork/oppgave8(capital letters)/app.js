d1294f97b71bb9cca8b015e2220654f58085dd50
Alper <alper.ccgn@gmail.com>
2023-06-10 14:15:47 +0200

    dom

---
+++ odev/teamwork/oppgave8(capital letters)/app.js
@@ -0,0 +1,10 @@
+//*Selectors****
+const textArea = document.querySelector("#textarea");
+const button = document.getElementById(btn)
+
+let par = document.createElement("p");
+
+
+button.addEventListener("click",()=>{
+    const text = document.createTextNode(textArea.value)
+})
