dfb0cb473a1ac2b25989d14747dfb077557a80a0
Alper <alper.ccgn@gmail.com>
2023-06-10 14:45:32 +0200

    ferdig

--- odev/teamwork/oppgave8(capital letters)/app.js
+++ odev/teamwork/oppgave8(capital letters)/app.js
@@ -1,10 +1,25 @@
 //*Selectors****
 const textArea = document.querySelector("#textarea");
-const button = document.getElementById(btn)
+const button = document.querySelector("#btn")
 
 let par = document.createElement("p");
 
 
 button.addEventListener("click",()=>{
-    const text = document.createTextNode(textArea.value)
+    const text = document.createTextNode(textArea.value);
+    par.appendChild(text);
+    button.after(par)
+    par.style.border="3px solid black"
+
+    const arrayText = textArea.value.split(" ")
+   
+
+    let a = "";
+    for(let i=0; i< arrayText.length;i++){
+        a += arrayText[i][0].toLocaleUpperCase()+arrayText[i].slice(1)+ " "
+    }
+    par.textContent = a
+    console.log(a);
+
+    
 })
