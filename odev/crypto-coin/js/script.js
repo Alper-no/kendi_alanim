3b947d45c1bf295639d413216cae90879ed7b3fe
Alper <alper.ccgn@gmail.com>
2023-06-17 15:50:36 +0200

    vs2

--- odev/crypto-coin/js/script.js
+++ odev/crypto-coin/js/script.js
@@ -1,21 +1,37 @@
-const API_KEY = 'coinrankingecf87f13e633a469857390e70e998373de6b66adcaaf87d5';
+const API_KEY = "coinrankingecf87f13e633a469857390e70e998373de6b66adcaaf87d5";
 
-const URL =`https://api.coinranking.com/v2/coins?api_key=${API_KEY}`;
+const URL = `https://api.coinranking.com/v2/coins?api_key=${API_KEY}`;
 
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    getCoin();
-})
-
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getCoin();
+});
 
 const getCoin = async () => {
-    try{
-        const reponse = await fetch(URL);
-        const data =await reponse.json();
-        console.log(data);
-    }catch(err){
-        console.log(err);
+  try {
+    const res = await fetch(URL);
+    if (!res.ok) {
+      throw new Error("Bulunmadi");
     }
+    const data = await reponse.json();
+    // console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const domaYaz = () => {
+  const coins = document.querySelector(".coins");
+  coins.innerHTML += `
+    <figure class="coin">
+          
+          <figcaption class="coin-name">
+            <p class="coin-temp"></p>
+            <sup></sup>
+          </figcaption>
+          <img class="coin-icon" src="" alt="">
+    </figure>
+        `;
 };
