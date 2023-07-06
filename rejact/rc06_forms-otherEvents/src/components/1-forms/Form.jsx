bec6eec0e20ef55d5d20f3e75667593f9ac38584
Alper <alper.ccgn@gmail.com>
2023-07-06 19:29:11 +0200

    r2

--- rejact/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
+++ rejact/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
@@ -1,5 +1,36 @@
 const Form = () => {
-  return <div className="text-danger">Form</div>
+  return (
+    <div className="container mt-4">
+      <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  )
 }
 
 export default Form
