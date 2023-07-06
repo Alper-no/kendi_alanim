b34cafea2089209319e421cfbc4656b836e6604c
Alper <alper.ccgn@gmail.com>
2023-07-06 20:17:16 +0200

    form2

--- rejact/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
+++ rejact/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
@@ -2,10 +2,25 @@ import { useState } from "react"
 
 const Form = () => {
   const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
   const handleUsername = (e) => {
     // console.log(e.target.value)
     setUsername(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    alert(`
+      username:${username}
+      email:${email}
+      password:${password}
+    `)
+    setEmail("")
+    setPassword("")
+    setUsername("")
   }
 
   return (
@@ -30,20 +45,28 @@ const Form = () => {
 
         <div className="mb-3">
           <label htmlFor="email" className="form-label">
-            Email address
+            Email address: <span className="text-danger">{email}</span>
           </label>
           <input
             type="email"
             className="form-control"
             id="email"
             aria-describedby="emailHelp"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">
             Password
           </label>
-          <input type="password" className="form-control" id="password" />
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
         </div>
 
         <div className="text-center">
