57ab4828ca817b8c18257974611664d7b0e33a27
Alper <alper.ccgn@gmail.com>
2023-07-06 19:52:45 +0200

    form

--- rejact/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
+++ rejact/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
@@ -1,33 +1,56 @@
+import { useState } from "react"
+
 const Form = () => {
+  const [username, setUsername] = useState("")
+
+  const handleUsername = (e) => {
+    // console.log(e.target.value)
+    setUsername(e.target.value)
+  }
+
   return (
     <div className="container mt-4">
+      <h2 className="text-center text-success">FORMS IN REACT</h2>
       <form>
         <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
+          <label htmlFor="username" className="form-label">
+            Hello {username}
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            aria-describedby="emailHelp"
+            //? OnChange event'ı input degeri her degistiginde tetiklenir. Biz de yazdıgımız event handler araciligi ile State'i guncelleyebilmis oluruz.
+            onChange={handleUsername}
+            value={username}
+            // value propu inputlara başlangıç değeri vermemizi sağlar. ve valu'a verilen değer değişken olmalıdır.
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
             Email address
           </label>
           <input
             type="email"
             className="form-control"
-            id="exampleInputEmail1"
+            id="email"
             aria-describedby="emailHelp"
           />
-          
         </div>
         <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
+          <label htmlFor="password" className="form-label">
             Password
           </label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-          />
+          <input type="password" className="form-control" id="password" />
         </div>
-        
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
+
+        <div className="text-center">
+          <button type="submit" className="btn btn-success">
+            Submit
+          </button>
+        </div>
       </form>
     </div>
   )
