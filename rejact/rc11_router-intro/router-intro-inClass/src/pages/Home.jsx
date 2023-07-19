2162d4961b49ff9843acaae934f849de7ce6be5d
Alper <alper.ccgn@gmail.com>
2023-07-19 19:48:27 +0200

    ROutes

--- rejact/rc11_router-intro/router-intro-inClass/src/pages/Home.jsx
+++ rejact/rc11_router-intro/router-intro-inClass/src/pages/Home.jsx
@@ -1,4 +1,8 @@
+//Routes: Konum her değiştiğinde, Routes en iyi eşleşmeyi bulmak için childları olan tüm alt Route öğelerine bakar ve kullanıcı arabiriminin bu dalını oluşturur.
+//Route: Url pathinden gelene göre hangi sayfanın(yani hangi componentin) render edileceğini belirtir.
+
 import About from "../components/About";
+
 import Courses from "../components/Courses";
 
 const Home = () => {
