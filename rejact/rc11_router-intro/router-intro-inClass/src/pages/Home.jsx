35f83972bf6a4ca45a824cbab6fe1bc8461dba67
Alper <alper.ccgn@gmail.com>
2023-07-19 21:31:04 +0200

    routes5

--- rejact/rc11_router-intro/router-intro-inClass/src/pages/Home.jsx
+++ rejact/rc11_router-intro/router-intro-inClass/src/pages/Home.jsx
@@ -1,15 +1,18 @@
 //Routes: Konum her değiştiğinde, Routes en iyi eşleşmeyi bulmak için childları olan tüm alt Route öğelerine bakar ve kullanıcı arabiriminin bu dalını oluşturur.
 //Route: Url pathinden gelene göre hangi sayfanın(yani hangi componentin) render edileceğini belirtir.
 
-import About from "../components/About";
-
-import Courses from "../components/Courses";
+import { Outlet } from "react-router-dom";
+// import About from "../components/About";
+// import Courses from "../components/Courses";
 
 const Home = () => {
   return (
     <div className="p-3">
-      <About />
-      <Courses />
+      <h1>Home Page</h1>
+      {/* <About /> */}
+      {/* <Courses /> */}
+      {/* //? nested route yapısında kullandığımız child componentleri Outlet componenti ile sergiliyoruz */}
+      <Outlet />
     </div>
   );
 };
