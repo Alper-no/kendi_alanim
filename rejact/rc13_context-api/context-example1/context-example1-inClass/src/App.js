2488c95ebbd0916b8b1d76cb08c9c48408709e83
Alper <alper.ccgn@gmail.com>
2023-07-22 12:24:28 +0200

    v3

--- rejact/rc13_context-api/context-example1/context-example1-inClass/src/App.js
+++ rejact/rc13_context-api/context-example1/context-example1-inClass/src/App.js
@@ -26,7 +26,7 @@ function App() {
 
           <Route path="people" element={<PrivateRouter />}>
             <Route path="" element={<People />} />
-            {/* <Route path="people/:id" element={<PersonDetail />} /> */}
+            <Route path="people/:id" element={<PersonDetail />} />
           </Route>
 
           <Route path="*" element={<Navigate to="/" />} />
