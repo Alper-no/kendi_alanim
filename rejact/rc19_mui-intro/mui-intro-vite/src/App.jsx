a8df5585bd22ef5176f4e83facac3c380aaafd13
Alper <alper.ccgn@gmail.com>
2023-08-03 21:35:25 +0200

    v3

--- rejact/rc19_mui-intro/mui-intro-vite/src/App.jsx
+++ rejact/rc19_mui-intro/mui-intro-vite/src/App.jsx
@@ -1,9 +1,13 @@
+import CardComp from "./components/CardComp";
+import TextFieldComp from "./components/TextField";
 import Typo from "./components/Typo";
 
 function App() {
   return (
     <>
       <Typo />
+      <TextFieldComp/>
+      <CardComp/>
     </>
   );
 }
