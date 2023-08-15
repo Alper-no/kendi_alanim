f673eaeb87f59a135ef7d57663d47d98f8381635
Alper <alper.ccgn@gmail.com>
2023-08-15 22:14:11 +0200

    toast

--- rejact/rc22-26_stock-app/stock-app-starter1/src/service/authApiCall.js
+++ rejact/rc22-26_stock-app/stock-app-starter1/src/service/authApiCall.js
@@ -1,10 +1,12 @@
 import axios from "axios";
+import {toastErrorNotify, toastSuccessNotify} from '../helper/ToastNotify'
 
 export const login = async (userData) => {
   const BASE_URL = "13554.fullstack.clarusway.com/";
   try {
     const { data } = await axios.post(`${BASE_URL}/account/login/`, userData)
-    console.log(data);
+    
+    toastSuccessNotify('Login Performed');
   } catch (error) {
     console.log(error);
   }
